2a3c8acc4e4646d7025bc866b5b48f34cd180e8d
Jose <josemorais9@protonmail.com>
2021-03-07 03:19:47 +0000

    logic to connect to the PUN server and user feedback

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
-	public GameObject roomSettingsPanel;
+	public GameObject RoomSettingsPanel;
+	public GameObject LoadingPanel;
+	public Animator logoAnimator;
+	public Text infoText;
+	public GameObject RetryButton;
 
 	private byte maxPlayersPerRoom = 4;
 	string gameVersion = "1";
@@ -16,16 +22,44 @@ public class Launcher : MonoBehaviourPunCallbacks
 
 	public void TriggerRoomSettings(bool value)
 	{
-		roomSettingsPanel.SetActive(value);
-	}
-
-	public void OnCreateMatch()
-	{
-
+		RoomSettingsPanel.SetActive(value);
 	}
 
 	public void Connect()
 	{
-		
+		if (PhotonNetwork.IsConnected)
+		{
+			// join room
+		} 
+		else
+		{
+			LoadingPanel.SetActive(true);
+			logoAnimator.enabled = true;
+			RetryButton.SetActive(false);
+			isConnecting = PhotonNetwork.ConnectUsingSettings();
+			PhotonNetwork.GameVersion = gameVersion;
+		}
+	}
+
+	public override void OnConnectedToMaster()
+	{
+		Debug.Log("Connecting Using Settings worked fine and I am now connected");
+		if (isConnecting)
+		{
+			isConnecting = false;
+			// create room
+		}
+
+	}
+
+	// Called after disconnecting from the Photon server. It could be a failure or an explicit disconnect call 
+	public override void OnDisconnected(DisconnectCause cause)
+	{
+		RetryButton.SetActive(true);
+		logoAnimator.enabled = false;
+		infoText.text = "Network connection failed";
+		infoText.color = Color.red;
+		isConnecting = false;
+		Debug.LogWarningFormat("Cause of disconnection: {0}", cause);
 	}
 }
