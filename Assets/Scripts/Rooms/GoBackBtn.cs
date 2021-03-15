16775d5d8d18e17275b9bcb72808bfa4342c5e51
Jose <josemorais9@protonmail.com>
2021-03-15 00:14:31 +0000

    Created Go Back button

---
+++ Assets/Scripts/Rooms/GoBackBtn.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoBackBtn : MonoBehaviour
+{
+    public void GoToPreviousScene()
+	{
+		int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(sceneIndex - 1);
+	}
+}
