c1a2af076a70cc870200592473ee19ee4d7b0410
Jose <josemorais9@protonmail.com>
2021-03-15 12:59:26 +0000

    OnRoomListUpdate callback to add new rooms to the list when they are created

---
+++ Assets/Scripts/Game Menu/Join Room Menu/RoomsList.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomsList : MonoBehaviourPunCallbacks
+{
+	[SerializeField] private Transform _content;
+	[SerializeField] private Room _room;
+
+	public override void OnRoomListUpdate(List<RoomInfo> roomList)
+	{
+		Debug.Log("onroomlistupdate from RoomsList");
+		foreach (RoomInfo info in roomList)
+		{
+			Room room = Instantiate(_room, _content);
+			if (room != null)
+			{
+				room.SetRoomInfo(info);
+			}
+		}
+	}
+}
