930fb440dcc1bbcd21239a0a964ae246619779d1
Jose <josemorais9@protonmail.com>
2021-03-15 12:59:59 +0000

    Method to change UI text to some room info

---
+++ Assets/Scripts/Game Menu/Join Room Menu/Room.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class Room : MonoBehaviour
+{
+    [SerializeField] private Text _text;
+
+    public void SetRoomInfo(RoomInfo roomInfo)
+	{
+		_text.text = roomInfo.MaxPlayers + ", " + roomInfo.Name;
+	}
+}
