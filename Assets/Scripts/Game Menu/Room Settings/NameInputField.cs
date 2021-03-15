afba65b4925f3dbf22112dd1ddbdd9bf786245b1
Jose <josemorais9@protonmail.com>
2021-03-15 00:08:23 +0000

    Script for the room name input field

---
+++ Assets/Scripts/Game Menu/Room Settings/NameInputField.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NameInputField : MonoBehaviour
+{
+    private InputField roomNameInputField;
+
+    private void Start()
+    {
+        roomNameInputField = GetComponent<InputField>();
+		roomNameInputField.text = PlayerPrefs.GetString(PlayerPrefsConstants.USERNAME) + "'s match";
+        RoomManager.Instance.RoomName = roomNameInputField.text;
+    }
+
+    public void OnEndEdit(string roomName)
+	{
+        RoomManager.Instance.RoomName = roomName;
+	}
+}
