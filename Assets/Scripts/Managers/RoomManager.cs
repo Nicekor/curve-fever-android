75166e10ca9372563623341a77024c52ccf6a9ea
Jose <josemorais9@protonmail.com>
2021-03-15 00:13:27 +0000

    Game and Room Managers to keep each context state across the app

---
+++ Assets/Scripts/Managers/RoomManager.cs
@@ -0,0 +1,8 @@
+﻿
+public class RoomManager : Singleton<RoomManager>
+{
+	public string RoomName { get; set; } = "";
+	public string RoomType { get; set; } = "PUBLIC";
+	public string RoomPassword { get; set; } = "";
+	public byte MaxPlayers { get; } = 4;
+}
