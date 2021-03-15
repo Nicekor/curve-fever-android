a5bac86de8900c43ce6555b73758b2f53af8aa33
Jose <josemorais9@protonmail.com>
2021-03-15 00:03:35 +0000

    Deleted CreateRoom script and using a button handler instead

---
+++ Assets/Scripts/Game Menu/CreateMatchBtnHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+public class CreateMatchBtnHandler : MonoBehaviourPunCallbacks
+{
+	public GameObject RoomSettings;
+	public Text roomName;
+
+	private const string ROOM_TYPE_PROP_KEY = "rt";
+	private const string ROOM_PW_PROP_KEY = "rp";
+
+	public void TriggerRoomSettings(bool value)
+	{
+		RoomSettings.SetActive(value);
+	}
+
+	public void CreateRoom()
+	{
+		// todo: instantiate the logo probablyyy
+		if (!PhotonNetwork.IsConnected)
+		{
+			return;
+		}
+		RoomOptions roomOptions = new RoomOptions();
+		roomOptions.MaxPlayers = RoomManager.Instance.MaxPlayers;
+		roomOptions.CustomRoomProperties = new Hashtable { { ROOM_TYPE_PROP_KEY, RoomManager.Instance.RoomType }, { ROOM_PW_PROP_KEY, RoomManager.Instance.RoomPassword } };
+		PhotonNetwork.CreateRoom(RoomManager.Instance.RoomName, roomOptions, TypedLobby.Default);
+	}
+
+	public override void OnCreatedRoom()
+	{
+		Debug.Log("Created room successfuly");
+	}
+
+	public override void OnCreateRoomFailed(short returnCode, string message)
+	{
+		Debug.LogWarningFormat("Room creation failed {0}", message);
+	}
+}
