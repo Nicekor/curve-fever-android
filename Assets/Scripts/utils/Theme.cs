824001d16781bd252549f9ed2c5a1de480c49b30
Jose <josemorais9@protonmail.com>
2021-03-16 18:34:12 +0000

    Created Theme script to store UI theme related data

---
+++ Assets/Scripts/utils/Theme.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Theme
+{
+    // buttons
+    public static Color primaryBtnBgColour = new Vector4(0.2039216f, 0.6392157f, 0.8078431f, 1f);
+    public static Color primaryBtnTextColour = new Vector4(1f, 1f, 1f, 1f);
+	public static Color accentBtnBgColour = new Vector4(0.8666667f, 0.9294118f, 0.1176471f, 1f);
+    public static Color accentBtnTextColour = new Vector4(0.04705882f, 0.2352941f, 0.145098f, 1f);
+    
+    // backgrounds
+    public static Color accentBgColour = new Vector4(0.02745098f, 0.2078432f, 0.3098039f, 1f);
+}
