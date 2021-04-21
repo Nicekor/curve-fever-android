537abfc8097d3ad85c51aa78c88480a6f433da60
Jose <josemorais9@protonmail.com>
2021-04-21 05:58:38 +0100

    Defined scene boundaries and fixed prefabs being instatiated after player collides

--- Assets/Scripts/Game/SnakeMovement.cs
+++ Assets/Scripts/Game/SnakeMovement.cs
@@ -1,58 +1,87 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
 
-public class SnakeMovement : MonoBehaviour
+public class SnakeMovement : MonoBehaviourPun
 {
-	public float speed = 3f;
-	public float rotationSpeed = 200f;
+    public float speed = 3f;
+    public float rotationSpeed = 200f;
 
-	private float horizontal = 0f;
+    private float horizontal = 0f;
+    private Dictionary<string, float> BOUNDARIES;
 
-	void Update()
-	{
-		SetHorizontal();
-	}
+    private void Awake()
+    {
+        float boundX = Camera.main.orthographicSize - 0.1f;
+        float boundY = Camera.main.orthographicSize * 2 - 0.1f;
+        BOUNDARIES = new Dictionary<string, float>() {
+            { "topX",  boundX},
+            { "botX", -boundX },
+            { "leftY", -boundY },
+            { "rightY", boundY },
+        };
+    }
 
-	void FixedUpdate()
-	{
-		transform.Translate(Vector2.up * speed * Time.fixedDeltaTime, Space.Self);
-		transform.Rotate(Vector3.forward * -horizontal * rotationSpeed * Time.fixedDeltaTime);
-	}
+    void Update()
+    {
+        SetHorizontal();
+    }
 
-	private void SetHorizontal()
-	{
-		if (Input.touchCount > 0)
-		{
-			Touch touch = Input.GetTouch(0);
+    void FixedUpdate()
+    {
+        if (photonView.IsMine)
+        {
+            // boundaries
+            if (transform.position.y <= BOUNDARIES["botX"] || transform.position.y >= BOUNDARIES["topX"] ||
+            transform.position.x <= BOUNDARIES["leftY"] || transform.position.x >= BOUNDARIES["rightY"])
+            {
+                Tail.Instance.StopDrawCoroutine();
+                speed = 0;
+                rotationSpeed = 0;
+            }
 
-			if (touch.phase == TouchPhase.Stationary)
-			{
-				if (touch.position.x > Screen.width / 2)
-				{
-					horizontal = 1f;
-				}
+            // input movement
+            transform.Translate(Vector2.up * speed * Time.fixedDeltaTime, Space.Self);
+            transform.Rotate(Vector3.forward * -horizontal * rotationSpeed * Time.fixedDeltaTime);
+        }
+    }
 
-				if (touch.position.x < Screen.width / 2)
-				{
-					horizontal = -1f;
-				}
-			}
-			else
-			{
-				horizontal = 0f;
-			}
+    private void SetHorizontal()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
 
-		}
-	}
+            if (touch.phase == TouchPhase.Stationary)
+            {
+                if (touch.position.x > Screen.width / 2)
+                {
+                    horizontal = 1f;
+                }
 
-	void OnTriggerEnter2D(Collider2D collision)
-	{
-		if (collision.CompareTag("killsPlayer"))
-		{
-			speed = 0;
-			rotationSpeed = 0;
-			// todo: use singleton instead of this (this is expensive)
-			FindObjectOfType<GameManager>().EndGame();
+                if (touch.position.x < Screen.width / 2)
+                {
+                    horizontal = -1f;
+                }
+            }
+            else
+            {
+                horizontal = 0f;
+            }
 
-		}
-	}
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("killsPlayer") && photonView.IsMine)
+        {
+            print("collision");
+            Tail.Instance.StopDrawCoroutine();
+            speed = 0;
+            rotationSpeed = 0;
+            // todo: use singleton instead of this (this is expensive)
+            //FindObjectOfType<GameManager>().EndGame();
+        }
+    }
 }
