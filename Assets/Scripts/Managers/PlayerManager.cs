036792da34aa52d055e4bd228fc00201c5117615
Jose <josemorais9@protonmail.com>
2021-04-21 05:56:38 +0100

    Created PlayerManager script to hold player related logic

---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : Singleton<PlayerManager>
+{
+    private Dictionary<string, Texture> avatarCache = new Dictionary<string, Texture>();
+    public Color[] avatarColours = { Color.red, Color.blue, Color.green, Color.yellow };
+
+    public void CacheTexture(string uri, Texture texture)
+    {
+        if (avatarCache.ContainsKey(uri)) return;
+        avatarCache.Add(uri, texture);
+    }
+
+    public Texture LookupTexture(string uri)
+    {
+        if (!avatarCache.ContainsKey(uri)) return null;
+        return avatarCache[uri];
+    }
+
+    public int GetRandomColourIndex()
+    {
+        return Random.Range(0, avatarColours.Length);
+    }
+}
