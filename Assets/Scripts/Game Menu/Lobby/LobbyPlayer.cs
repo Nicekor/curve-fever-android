f34aa2ceb8785afc1411aa5d07d3ac03f1676f2b
Jose <josemorais9@protonmail.com>
2021-04-21 06:00:40 +0100

    Room's lobby logic

--- Assets/Scripts/Game Menu/Lobby/LobbyPlayer.cs
+++ Assets/Scripts/Game Menu/Lobby/LobbyPlayer.cs
@@ -1,26 +1,54 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using Photon.Realtime;
+using Photon.Pun;
 using System.Collections;
 using UnityEngine.Networking;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
-public class LobbyPlayer : MonoBehaviour
+public class LobbyPlayer : MonoBehaviourPunCallbacks
 {
-	[SerializeField] private RawImage avatar;
-	[SerializeField] private Text text;
+    [SerializeField] private RawImage avatar;
+    [SerializeField] private Text text;
+    public RawImage checkIcon;
 
-	public Player Player { get; private set; }
+    public Player Player { get; private set; }
+    public bool Ready = false;
 
-    // generate a color for each player to get the avatar and this color will persist for the snake
-	public void SetPlayerInfo(Player player)
-	{
-		Player = player;
-		text.text = player.NickName;
-        StartCoroutine(DownloadImage("https://eu.ui-avatars.com/api/?name=" + player.NickName + "&length=1&background=random"));
+    public void SetPlayerInfo(Player player)
+    {
+        Player = player;
+        text.text = player.NickName;
+        if (player.CustomProperties.ContainsKey("colour"))
+        {
+            StartCoroutine(DownloadAvatar(getPlayerAvatarUri(player.NickName, (string)player.CustomProperties["colour"])));
+        }
     }
 
-    IEnumerator DownloadImage(string uri)
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
     {
+        if (targetPlayer != null && targetPlayer == Player)
+        {
+            if (changedProps.ContainsKey("colour"))
+            {
+                StartCoroutine(DownloadAvatar(getPlayerAvatarUri(targetPlayer.NickName, (string)targetPlayer.CustomProperties["colour"])));
+            }
+        }
+    }
+
+    private string getPlayerAvatarUri(string nickname, string colour)
+    {
+        return "https://eu.ui-avatars.com/api/?name=" + nickname + "&length=1&background=" + colour;
+    }
+
+    IEnumerator DownloadAvatar(string uri)
+    {
+        Texture cachedTexture = PlayerManager.Instance.LookupTexture(uri);
+        if (cachedTexture != null)
+        {
+            renderAvatar(cachedTexture);
+            yield return null;
+        }
         UnityWebRequest request = UnityWebRequestTexture.GetTexture(uri);
         yield return request.SendWebRequest();
 
@@ -30,8 +58,15 @@ public class LobbyPlayer : MonoBehaviour
         }
         else
         {
-            avatar.color = Color.white;
-            avatar.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+            Texture texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+            PlayerManager.Instance.CacheTexture(uri, texture);
+            renderAvatar(texture);
         }
+    }
+
+    private void renderAvatar(Texture texture)
+    {
+        avatar.color = Color.white;
+        avatar.texture = texture;
     }
 }
