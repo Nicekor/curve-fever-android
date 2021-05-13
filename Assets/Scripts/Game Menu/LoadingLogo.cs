704ccf6fdea7058527485dcae18530b4a04c6de8
Jose <josemorais9@protonmail.com>
2021-05-14 00:56:47 +0100

    Created LoadingLogo prefab and script to Instantiate when needed

---
+++ Assets/Scripts/Game Menu/LoadingLogo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingLogo : MonoBehaviour
+{
+	[SerializeField] private Text _infoText;
+	[SerializeField] private GameObject _retryBtn;
+	[SerializeField] private Animator _logoAnimator;
+	public Text InfoText { get { return _infoText; } }
+	public GameObject RetryBtn { get { return _retryBtn; } }
+	public Animator LogoAnimator { get { return _logoAnimator; } }
+}
