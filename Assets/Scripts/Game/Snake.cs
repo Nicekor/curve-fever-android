8b57315710953507f40ef473d8da5a58dc4a4228
Jose <josemorais9@protonmail.com>
2021-05-14 01:00:04 +0100

    Logic to load player colour and its respective head sprite

---
+++ Assets/Scripts/Game/Snake.cs
@@ -0,0 +1,36 @@
+﻿using Photon.Pun;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviourPunCallbacks, IPunInstantiateMagicCallback
+{
+    private Photon.Realtime.Player player;
+
+    [SerializeField]
+    private Dictionary<Color, string> headSpritesPaths = new Dictionary<Color, string>()
+        {
+            { Color.red, "Sprites/red_head" },
+            { Color.blue, "Sprites/blue_head" },
+            { Color.green, "Sprites/green_head" },
+            { Color.yellow, "Sprites/yellow_head" },
+        };
+
+    public void OnPhotonInstantiate(PhotonMessageInfo info)
+    {
+        player = info.Sender;
+    }
+
+    public Color GetPlayerColour()
+    {
+        if (player == null) return Color.white;
+        string colourHex = (string)player.CustomProperties["colour"];
+        return PlayerManager.Instance.HexToColour(colourHex);
+    }
+
+    public string GetHeadSpritePath(Color colour)
+    {
+        //if (player == null || colour == null) return headSpritesPaths[Color.red];
+        print(colour.ToString());
+        return headSpritesPaths[GetPlayerColour()];
+    }
+}
