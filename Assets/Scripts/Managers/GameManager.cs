84706866c1a34b4d1aad450e95c4e105546967f8
Jose <josemorais9@protonmail.com>
2021-05-14 01:00:28 +0100

    Instantiating the player in the network

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
 public class GameManager : Singleton<GameManager>
 {
+	public GameObject playerPrefab;
 	private bool hasEnded = false;
 
-	public void EndGame()
+    private void Start()
+    {
+		// todo: random position
+		int xPos = Random.Range(-8, 9);
+		int yPos = Random.Range(-4, 4);
+        PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(xPos, yPos, 0), Quaternion.identity, 0);
+    }
+
+    public void EndGame()
 	{
 		if (hasEnded) return;
 		hasEnded = true;
