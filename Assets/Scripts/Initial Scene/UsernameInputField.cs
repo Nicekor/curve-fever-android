b4656f3bae4970aa3c327f1a63e08d39144ece95
Jose <josemorais9@protonmail.com>
2021-05-17 18:26:32 +0100

    username validation

--- Assets/Scripts/Initial Scene/UsernameInputField.cs
+++ Assets/Scripts/Initial Scene/UsernameInputField.cs
@@ -5,6 +5,14 @@ using Photon.Pun;
 [RequireComponent(typeof(InputField))]
 public class UsernameInputField : MonoBehaviour
 {
+	[SerializeField] private GameObject emptyUsernameText;
+	[SerializeField] private Button customGamesBtn;
+
+	private void OnEnable()
+	{
+		customGamesBtn.onClick.AddListener(OnClick_CustomGamesBtn);
+	}
+
 	private void Start()
 	{
 		string defaultName = string.Empty;
@@ -22,16 +30,32 @@ public class UsernameInputField : MonoBehaviour
 		PhotonNetwork.NickName = defaultName;
 	}
 
+	private void OnClick_CustomGamesBtn()
+	{
+		if (!IsUsernameValid(PlayerPrefs.GetString(PlayerPrefsConstants.USERNAME))) return;
+
+		SceneLoader.Instance.LoadScene(1);
+	}
+
+	private bool IsUsernameValid(string username)
+	{
+		if (string.IsNullOrEmpty(username.Trim()))
+		{
+			emptyUsernameText.SetActive(true);
+			return false;
+		}
+
+		emptyUsernameText.SetActive(false);
+		return true;
+	}
+
 	public void SetUsername(string username)
 	{
-		if (string.IsNullOrEmpty(username))
-		{
-			// todo: Add this to a gui label
-			Debug.LogError("Player Name is null or empty");
-			return;
-		}
-		PhotonNetwork.NickName = username;
-
 		PlayerPrefs.SetString(PlayerPrefsConstants.USERNAME, username);
+
+		if (!IsUsernameValid(username)) return;
+
+		PhotonNetwork.NickName = username;
 	}
+
 }
