2d2a65f5a09f962701491fc88e61036fd941f738
Jose <josemorais9@protonmail.com>
2021-05-19 20:37:37 +0100

    Tweaks on Loading Logo and SceneLoader

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class SceneLoader : Singleton<SceneLoader>
 {
 	public GameObject loadingScreen;
-	public Slider slider;
 	public Text progressText;
 
     public void LoadScene(int sceneIndex)
@@ -22,7 +21,6 @@ public class SceneLoader : Singleton<SceneLoader>
 		while(!operation.isDone)
 		{
 			float progress = Mathf.Clamp01(operation.progress / .9f);
-			slider.value = progress;
 			progressText.text = progress * 100f + "%";
 			yield return null;
 		}
