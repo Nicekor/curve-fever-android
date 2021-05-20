0b930b00f6cd579a7265efa62ed0ad09d73d8815
Jose <josemorais9@protonmail.com>
2021-05-20 02:33:36 +0100

    Leaderboard try

---
+++ Assets/Scripts/Game/UI/LeaderboardPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class LeaderboardPlayer : MonoBehaviour
+{
+	[SerializeField] private Text placeText;
+	[SerializeField] private Text nicknameText;
+
+	public void SetLeaderboardPlayerInfo(int playerPosition, Player player)
+	{
+		placeText.text = playerPosition.ToString() + ".";
+		nicknameText.text = player.NickName;
+	}
+}
